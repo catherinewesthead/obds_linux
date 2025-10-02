2fcdab2f9482379799c3ce047fda7a2e2b563a5a
catherinewesthead <c.a.westhead@googlemail.com>
2025-10-02 10:57:13 +0100

    Update rnaseq_commands.sh
    Added code for fastqc, multiqc

--- rnaseq_commands.sh
+++ rnaseq_commands.sh
@@ -1,1 +1,12 @@
 /project/sedm6772/linux/3_analysis/rnaseq_commands.sh
+
+#Fastqc code:
+fastqc /project/sedm6772/linux/2_rnaseq/1_fastq/fastq_pair/cd4_rep1_read1.fastq.gz -o /project/sedm6772/linux/3_analysis/1_fastqc/
+
+fastqc /project/sedm6772/linux/2_rnaseq/1_fastq/fastq_pair/cd4_rep1_read2.fastq.gz -o /project/sedm6772/linux/3_analysis/1_fastqc/
+
+#multiqc code
+mutliqc /project/sedm6772/linux/3_analysis/1_fastqc/cd4_rep1_read1_fastqc.zip /project/sedm6772/linux/3_analysis/1_fastqc/cd4_rep1_read2_fastqc.zip -o /project/sedm6772/linux/3_analysis/reports
+
+#copying files from ssh to my local computer 
+scp obds:/project/sedm6772/linux/3_analysis/1_fastqc/cd4_rep1_read1_fastqc.html .
