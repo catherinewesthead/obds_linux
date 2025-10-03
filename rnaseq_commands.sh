e03c7c761d7c1c5526d8bf68825cbad58137efea
catherinewesthead <c.a.westhead@googlemail.com>
2025-10-03 15:23:22 +0100

    Update rnaseq_commands.sh

--- rnaseq_commands.sh
+++ rnaseq_commands.sh
@@ -72,5 +72,58 @@ hisat2 --threads 8 \
 
 # it is important to match the number of threads to the --ntasks= specified!
 
+# read mapping makes sam files - these need to be converted to bam files for further steps
+# this required the following slurm script
+#!/bin/bash
+##########################################################################
+## A script template for submitting batch jobs. To submit a batch job,
+## please type
+##
+##    sbatch script_name.sh
+##
+## Please note that anything after the characters "#SBATCH" on a line
+## will be treated as a Slurm option.
+##########################################################################
 
+## Specify a partition. Check available partitions using sinfo Slurm command.
+#SBATCH --partition=short
+
+## The following line will send an email notification to your registered email
+## address when the job ends or fails.
+#SBATCH --mail-type=END,FAIL
+
+## Specify the amount of memory that your job needs. This is for the whole job.
+## Asking for much more memory than needed will mean that it takes longer to
+## start when the cluster is busy.
+#SBATCH --mem=10G
+
+## Specify the number of CPU cores that your job can use. This is only relevant for
+## jobs which are able to take advantage of additional CPU cores. Asking for more
+## cores than your job can use will mean that it takes longer to start when the
+## cluster is busy.
+#SBATCH --ntasks=4
+
+## Specify the maximum amount of time that your job will need to run. Asking for
+## the correct amount of time can help to get your job to start quicker. Time is
+## specified as DAYS-HOURS:MINUTES:SECONDS. This example is one hour.
+#SBATCH --time=0-01:00:00
+
+## Provide file name (files will be saved in directory where job was ran) or path
+## to capture the terminal output and save any error messages. This is very useful
+## if you have problems and need to ask for help.
+#SBATCH --output=samtools_%x.out
+#SBATCH --error=samtools_%x.err
+## ################### CODE TO RUN ##########################
+# Load modules (if required - e.g. when not using conda)
+# module load R-base/4.3.0
+# Execute these commands
+
+
+samtools view --threads 4 -b aln-pe.sam > aln-pe.bam &&
+samtools sort --threads 4 aln-pe.bam > sorted_aln-pe.bam &&
+samtools index sorted_aln-pe.bam &&
+samtools flagstat sorted_aln-pe.bam > sorted_aln-pe.flagstat &&
+samtools idxstats sorted_aln-pe.bam > sorted_aln-pe.idxstats
+
+# multiqc can be used to visualise outputs
 
