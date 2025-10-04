d85a8fc8b88cef0458868d64b205354f49e641b6
catherinewesthead <c.a.westhead@googlemail.com>
2025-10-04 17:49:37 +0100

    Update rnaseq_commands.sh

--- rnaseq_commands.sh
+++ rnaseq_commands.sh
@@ -126,4 +126,59 @@ samtools flagstat sorted_aln-pe.bam > sorted_aln-pe.flagstat &&
 samtools idxstats sorted_aln-pe.bam > sorted_aln-pe.idxstats
 
 # multiqc can be used to visualise outputs
+# to run multiqc the command is simple and can be conducted on the cluster 
+multiqc <file path> e.g., /project/sedm6772/limux/3_analysis 
+# this command should find relevant files
+
+
+#!/bin/bash
+##########################################################################
+## A script template for submitting batch jobs. To submit a batch job,
+## please type
+##
+##    sbatch script_name.sh
+##
+## Please note that anything after the characters "#SBATCH" on a line
+## will be treated as a Slurm option.
+##########################################################################
+
+## Specify a partition. Check available partitions using sinfo Slurm command.
+#SBATCH --partition=short
+
+## The following line will send an email notification to your registered email
+## address when the job ends or fails.
+#SBATCH --mail-type=END,FAIL
+
+## Specify the amount of memory that your job needs. This is for the whole job.
+## Asking for much more memory than needed will mean that it takes longer to
+## start when the cluster is busy.
+#SBATCH --mem=10G
+
+## Specify the number of CPU cores that your job can use. This is only relevant for
+## jobs which are able to take advantage of additional CPU cores. Asking for more
+## cores than your job can use will mean that it takes longer to start when the
+## cluster is busy.
+#SBATCH --ntasks=1
+
+## Specify the maximum amount of time that your job will need to run. Asking for
+## the correct amount of time can help to get your job to start quicker. Time is
+## specified as DAYS-HOURS:MINUTES:SECONDS. This example is one hour.
+#SBATCH --time=0-01:00:00
+
+## Provide file name (files will be saved in directory where job was ran) or path
+## to capture the terminal output and save any error messages. This is very useful
+## if you have problems and need to ask for help.
+#SBATCH --output=featurecounts_.out
+#SBATCH --error=featurecounts_.err
+## ################### CODE TO RUN ##########################
+# Load modules (if required - e.g. when not using conda)
+# module load R-base/4.3.0
+
+# Execute these commands
+
+
+featureCounts -t exon -g gene_id -p -a /project/sedm6772/linux/2_rnaseq/2_genome/Mus_musculus.GRCm39.115.gtf.gz -o counts.txt \
+/project/sedm6772/linux/3_analysis/sorted_aln-pe.bam
+
+
 
